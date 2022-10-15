c0439859c9e4aa47ec38310d889a38a82e3ae13a
Fabio Teraoka <65736019+fabioteraoka@users.noreply.github.com>
2022-10-14 22:17:54 -0300

    Signin

---
+++ src/voar.tsx
@@ -0,0 +1,10 @@
+export function Voar() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="250.000000pt"
+      height="205.000000pt"
+      viewBox="0 0 250.000000 205.000000"
+    />
+  )
+}
